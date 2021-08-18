81731d6bef9d30c3e82727da552cedbdddcfd698
Chirag <cgupta7.rag2101@gmail.com>
2021-08-19 01:14:54 +0530

    almost done

--- game.js
+++ game.js
@@ -1,4 +1,6 @@
 var userClickedPattern=[];
+
+var started=false;
 var level=0;
 
 var gamePattern=[];
@@ -6,36 +8,79 @@ var gamePattern=[];
 
 var buttonColours=["red","blue", "green", "yellow"];
 
-$(document).keypress(nextSequence);
-   
+
 $(".btn").click(function(){
     var userChosenColour=$(this).attr("id");
     userClickedPattern.push(userChosenColour);
     
     var clickAudio = new Audio('sounds/'+userChosenColour+'.mp3');
     clickAudio.play();
-
+    
     $("#" + userChosenColour).addClass("pressed");
-
-  setTimeout(function () {
-    $("#" + userChosenColour).removeClass("pressed");
-  }, 100);
-
+    
+    setTimeout(function () {
+        $("#" + userChosenColour).removeClass("pressed");
+    }, 100);
+    
+    checkAnswer(userClickedPattern.length-1);
 });
 
+$(document).keypress(function() {
+    if (!started) {
+  
+      //3. The h1 title starts out saying "Press A Key to Start", when the game has started, change this to say "Level 0".
+      $("#level-title").text("Level " + level);
+      nextSequence();
+      started = true;
+    }
+  });
+  
 
-nextSequence()
+function nextSequence()
 {
+    userClickedPattern=[];
     level++;
-    $("#level-title").text("Level"+level);
+    $("#level-title").text("Level "+level);
     var randomNumber=Math.floor(Math.random()*4);
     var randomChosenColour = buttonColours[randomNumber];
     gamePattern.push(randomChosenColour);
-    
+   
     $("#"+randomChosenColour).fadeOut(100).fadeIn(100);
     var audio = new Audio('sounds/'+randomChosenColour+'.mp3');
     audio.play();
-
+    
 }
 
 
+// if (randomChosenColour===userChosenColour) {
+//     started=false;
+//     if (!started) {
+  
+//         nextSequence();
+//         started = true;
+//       }
+// }
+// else if (started) {
+    
+//     $("#level-title").text("You Loose");
+// }
+
+
+
+function checkAnswer(currentLevel){
+    if(userClickedPattern[currentLevel]===gamePattern[currentLevel]){
+        console.log("success");
+        if (userClickedPattern.length === gamePattern.length){
+
+            setTimeout(function () {
+              nextSequence();
+            }, 800);
+    
+          }
+    
+    }
+    else {console.log("failed");
+    
+    }
+
+}
