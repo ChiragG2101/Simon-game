864918ac4f4100036d8e45ea8a075f0530d59063
Chirag <cgupta7.rag2101@gmail.com>
2021-08-18 23:28:06 +0530

    step 4

--- game.js
+++ game.js
@@ -1,9 +1,29 @@
+userClickedPattern=[];
+
+
+var gamePattern=[];
+
+
+var buttonColours=["red","blue", "green", "yellow"];
+
+$(".btn").click(function(){
+    var userChosenColour=$(this).attr("id");
+    userClickedPattern.push(userChosenColour);
+    
+    var clickAudio = new Audio('sounds/'+userChosenColour+'.mp3');
+    clickAudio.play();
+});
+
+
 nextSequence()
 {
     var randomNumber=Math.floor(Math.random()*4);
+    var randomChosenColour = buttonColours[randomNumber];
+    gamePattern.push(randomChosenColour);
+    
+    $("#"+randomChosenColour).fadeOut(100).fadeIn(100);
+    var audio = new Audio('sounds/'+randomChosenColour+'.mp3');
+    audio.play();
 }
-var buttonColours=["red","blue", "green", "yellow"];
 
-var randomChosenColour = buttonColours[randomNumber];
-var gamePattern=[];
-gamePattern.push(randomChosenColour);
+
